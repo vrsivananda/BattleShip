f6fb377c853bd1b774631d01b9afe0138d31777c
Sivananda Rajananda <Sivananda Rajananda>
2017-05-25 23:02:24 -0700

    Added createOptionsStore.m, fixed bugs on setup BSPS.m

--- checkRange.m
+++ checkRange.m
@@ -1,10 +1,19 @@
 function output = checkRange(headPlace, tailPlace, shipBoard)
-    if(headPlace(1) ~= tailPlace(1))
-        range = shipBoard(headPlace(1):(tailPlace(1)-headPlace(1)):tailPlace(1),headPlace(2),headPlace(3));
-    elseif(headPlace(2) ~= tailPlace(2))
-        range = shipBoard(headPlace(1),headPlace(2):(tailPlace(2)-headPlace(2)):tailPlace(2),headPlace(3));
-    elseif(headPlace(3) ~= tailPlace(3))
-        range = shipBoard(headPlace(1),headPlace(2),headPlace(3):(tailPlace(3)-headPlace(3)):tailPlace(3));
+    
+    %If head < tail, start from head
+    
+    if(headPlace(1) < tailPlace(1))
+        range = shipBoard(headPlace(1):tailPlace(1),headPlace(2),headPlace(3));
+    elseif(headPlace(1) > tailPlace(1))
+        range = shipBoard(tailPlace(1):headPlace(1),headPlace(2),headPlace(3));
+    elseif(headPlace(2) < tailPlace(2))
+        range = shipBoard(headPlace(1),headPlace(2):tailPlace(2),headPlace(3));
+    elseif(headPlace(2) > tailPlace(2))
+        range = shipBoard(headPlace(1),tailPlace(2):headPlace(2),headPlace(3));
+    elseif(headPlace(3) < tailPlace(3))
+        range = shipBoard(headPlace(1),headPlace(2),headPlace(3):tailPlace(3));
+    elseif(headPlace(3) > tailPlace(3))
+        range = shipBoard(headPlace(1),headPlace(2),tailPlace(3):tailPlace(3));
     else
         range = 1;
     end
---
+++ createOptionsStore.m
@@ -0,0 +1,38 @@
+function output = createOptionsStore(headPlace, shipBoard)
+
+    optionsStore = [];
+                    
+    %Check downwards
+    if(headPlace(1)+theLength-1 <= rows && checkRange(headPlace,[headPlace(1)+theLength-1, headPlace(2), headPlace(3)],shipBoard))
+        optionsStore(size(optionsStore,1)+1,:) = [headPlace(1)+theLength-1, headPlace(2), headPlace(3)];
+    end
+
+    %Check upwards
+    if (headPlace(1)-theLength+1 >= 1 && checkRange(headPlace, [headPlace(1)-theLength+1, headPlace(2), headPlace(3)], shipBoard))
+        optionsStore(size(optionsStore,1)+1,:) = [headPlace(1)-theLength+1, headPlace(2), headPlace(3)];
+    end
+
+    %Check rightwards
+    if(headPlace(2)+theLength-1 <= cols && checkRange(headPlace, [headPlace(1), headPlace(2)+theLength-1, headPlace(3)], shipBoard))
+        optionsStore(size(optionsStore,1)+1,:) = [headPlace(1), headPlace(2)+theLength-1, headPlace(3)];
+    end
+
+    %Check leftwards
+    if (headPlace(2)-theLength+1 >= 1 && checkRange(headPlace, [headPlace(1), headPlace(2)-theLength+1, headPlace(3)], shipBoard))
+        optionsStore(size(optionsStore,1)+1,:) = [headPlace(1), headPlace(2)-theLength+1, headPlace(3)];
+    end
+    
+% Commented out because we cannot go through sheets
+%     %Check inwards
+%     if(headPlace(3)+theLength-1 <= sheets && checkRange(headPlace, [headPlace(1), headPlace(2), headPlace(3)+theLength-1], shipBoard))
+%         optionsStore(size(optionsStore,1)+1,:) = [headPlace(1), headPlace(2), headPlace(3)+theLength-1];
+%     end
+% 
+%     %Check outwards
+%     if (headPlace(3)-theLength-1 >= 0 && checkRange(headPlace, [headPlace(1), headPlace(2), headPlace(3)-theLength-1], shipBoard))
+%         optionsStore(size(optionsStore,1)+1,:) = [headPlace(1), headPlace(2), headPlace(3)-theLength-1];
+%     end
+
+    output = optionsStore;
+
+end
